44b3d989d88aa7adc6eaf259abe1c3269b6b656c
railagan <railagan@philweb.com.ph>
2014-02-17 16:19:46 +0800

    initial commit

---
+++ app/view/NotesList.js
@@ -0,0 +1,11 @@
+Ext.define("senchanotes.view.NotesList", {
+    extend: "Ext.dataview.List",
+    alias: "widget.noteslist",
+    config: {
+        loadingText: "Loading Notes...",
+        emptyText: "</pre><div class='notes-list-empty-text'>No notes found.</div><pre>",
+        onItemDisclosure: true,
+        grouped: true,
+        itemTpl: '</pre><div class="list-item-title">{title}</div><div class="list-item-narrative">{narrative}</div><pre>'
+    }
+});
